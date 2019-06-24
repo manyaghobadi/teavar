ada7505620e9e72eebc8f70f4c3957f7d4d61b54
Jeremy Bogle <jbogle@mit.edu>
2019-06-24 19:52:45 -0400

    Fix demo path

---
+++ www/demo/precache-manifest.60c1748a60368ec81fc00fbbb88644c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdfd4454a02eef365ddf",
+    "url": "./static/css/main.073e0186.chunk.css"
+  },
+  {
+    "revision": "bdfd4454a02eef365ddf",
+    "url": "./static/js/main.56ad90c6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a9ca45ae910f1e2b0a0f",
+    "url": "./static/js/2.99aa4106.chunk.js"
+  },
+  {
+    "revision": "136dff91c21bef14a8922cc0c121f1fd",
+    "url": "./index.html"
+  }
+];
