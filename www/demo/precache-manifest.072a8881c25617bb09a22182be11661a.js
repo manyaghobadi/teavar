877f1a204874b0973039aeb8e920613c509c19bc
Jeremy Bogle <jbogle@mit.edu>
2019-08-12 16:48:50 -0400

    Diagram, code builds, updated demo

---
+++ www/demo/precache-manifest.072a8881c25617bb09a22182be11661a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fbfaa2e01c5ed43abaf",
+    "url": "./static/css/main.3e3b9dba.chunk.css"
+  },
+  {
+    "revision": "1fbfaa2e01c5ed43abaf",
+    "url": "./static/js/main.4c9cff57.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "994a83da428ad2a88471",
+    "url": "./static/js/2.d5a8389c.chunk.js"
+  },
+  {
+    "revision": "4bd9a6c0ed2ae5b23da0219cef1cc493",
+    "url": "./index.html"
+  }
+];
